746627552a731c480810c7c2b2a71878045e9688
Pratyay Gaurav <77407830+pratyaygaurav@users.noreply.github.com>
2022-04-29 22:02:15 +0530

    Add files via upload

---
+++ src/Sdata.jsx
@@ -0,0 +1,31 @@
+const Sdata = [
+    {
+    imgsrc : "https://wallpapercave.com/wp/wp4056410.jpg", 
+    title : "A Netflix Original Series",
+    sname  :  'DARK',
+    link : "https://www.netflix.com/in/tit;e/80990668?source=35",
+        
+       
+    },
+
+    {
+         imgsrc : "https://wallpapercave.com/wp/wp5152695.jpg" ,
+         title : "A Netflix Original Series",
+        sname  :  'Sex Education',
+        link : "https://www.netflix.com/in/tit;e/80990668?source=35",
+       
+       
+        
+
+    },
+
+    {
+       imgsrc : "https://wallpapercave.com/wp/wp7948301.jpg" ,
+       title : "A Netflix Original Series",
+        sname  :  'Queens Gambit',
+        link : "https://www.netflix.com/in/tit;e/80990668?source=35" 
+        
+    },
+]
+
+export default Sdata;
